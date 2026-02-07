f2b63ae370a353de62df26afd39a4c3362997832
fereidoon <ferydoon@gmail.com>
2026-02-07 16:09:37 +0330

    Add news and announcement pages

--- apps/home/views.py
+++ apps/home/views.py
@@ -5,6 +5,8 @@ Views for the home app.
 from django.shortcuts import render
 from wagtail.models import Page
 
+from .models import AnnouncementPage, NewsPage
+
 
 def home(request):
     """
@@ -15,6 +17,11 @@ def home(request):
     except Page.DoesNotExist:
         home_page = None
     
+    news_items = NewsPage.objects.live().order_by('-first_published_at')[:3]
+    announcement_items = AnnouncementPage.objects.live().order_by('-first_published_at')[:3]
+
     return render(request, 'home/home.html', {
         'page': home_page,
+        'news_items': news_items,
+        'announcement_items': announcement_items,
     })
